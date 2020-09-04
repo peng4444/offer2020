d3cbad9f095ce1fed46f9b73f1492494dc105d27
peng4444 <pengbingjiang4@163.com>
2020-09-04 22:09:25 +0800

    20200904-1.some offer interview update          2.some leetcode demo update and push.

--- offer_demo/src/main/java/cn/offer2020/pbj/demo/TestDemo.java
+++ offer_demo/src/main/java/cn/offer2020/pbj/demo/TestDemo.java
@@ -1,33 +1,41 @@
 package cn.offer2020.pbj.demo;
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestDemo{
+    public static int[] SpiralMatrix (int[][] matrix) {
+        // write code here
+        int col = matrix.length;
+        int row = matrix[0].length;
+        if(col==0||row==0) return new int[]{};
+        int len = col*row;
+        int[] ans = new int[len];
+        List<Integer> list = new ArrayList<>();
+        int index = 0;
+        while(index<(col+1)/2&&index<(row+1)/2){
+            for(int r = index;r<row-index;r++){
+                list.add(matrix[index][r]);
+            }
+            for(int c = index+1;c<col-index;c++){
+                list.add(matrix[c][row-index-1]);
+            }
+            for(int r = row-index-2;r>index&&col-index-1>index;r--){
+                list.add(matrix[col-index-1][r]);
+            }
+            for(int c = col-index-1;c>index&&index<row-index-1;c--){
+                list.add(matrix[c][index]);
+            }
+            index++;
+        }
+        for(int i = 0;i<list.size();i++){
+            ans[i] = list.get(i);
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] nums = new int[n];
-        int zero = 0;
-        int one = 0;
-        for (int i = 0; i < n; i++) {
-            nums[i] = sc.nextInt();
-            if (nums[i] == 0) zero++;
-            if (nums[i] == 5) one++;
-        }
-        StringBuilder sb = new StringBuilder();
-        //长度很大
-        if (zero == 0)
-            System.out.println(-1);
-        if (zero == 1 && one < 9)
-            System.out.println(-1);
-        if (zero >= 1 && one > 9) {
-            for (int i = 0; i < 9; i++) {
-                sb.append("5");
-            }
-            for (int i = 0; i < zero; i++) {
-                sb.append("0");
-            }
-            System.out.println(sb.toString());
-        }
+        int[][] num = new int[][]{{1,2,3,4} ,{ 5, 6, 7,8} , {9,10,11,12}};
+        SpiralMatrix(num);
     }
 }
