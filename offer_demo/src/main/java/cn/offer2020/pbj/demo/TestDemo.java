55dbcf8ed1c72d1357f10f903dddac7d57f3b4ba
peng4444 <pengbingjiang4@163.com>
2020-09-08 22:07:38 +0800

    20200908-1.some offer interview update          2.some leetcode demo update and push.

--- offer_demo/src/main/java/cn/offer2020/pbj/demo/TestDemo.java
+++ offer_demo/src/main/java/cn/offer2020/pbj/demo/TestDemo.java
@@ -3,41 +3,38 @@ package cn.offer2020.pbj.demo;
 import java.util.*;
 
 public class TestDemo {
-    public static void main1(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        String str = sc.nextLine();
-        String[] s = str.split(" ");
-        String[] nums1 = new String[2*T];
-        String[] nums2 = new String[2*T];
-        int index = 0;
-        for(int i = 0;i<str.length();i=i+4){
-            nums1[index] = s[i];
-            nums1[index+1] = s[i+1];
-            nums2[index] = s[i+2];
-            nums2[index+1] = s[i+3];
-            index=index+2;
-        }
-    }
-
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        String str = sc.next();
-        char[] chars = str.toCharArray();
-        int[] nums = new int[128];
-        for(int i = 0;i<n;i++){
-            nums[chars[i]-'a']++;
-        }
-        int two = 0;
-        for(int i = 0;i<nums.length;i++){
-            if(nums[i]>1) two++;
-        }
-        if(two==0){
-            System.out.println(n);
-        }else{
-            System.out.println(n-two+1);
-        }
+        String str = sc.nextLine();
+        char[][] chars = new char[][]{{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
+        System.out.println(help(chars,str));
+        //System.out.println(true);
+    }
 
+    public static boolean help(char[][] chars,String str){
+        boolean[][] visited = new boolean[chars.length][chars[0].length];
+        for(int i = 0;i<chars.length;i++){
+            for(int j = 0;j<chars[0].length;j++){
+                if(dfs(chars,str,i,j,0,visited)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean dfs(char[][] chars,String str,int i,int j,int index,boolean[][] visited){
+        if(index==str.length()){
+            return true;
+        }
+        if(i<0||i>=chars.length||j<0||j>=chars[0].length||chars[i][j]!=str.charAt(index)||visited[i][j]==true){
+            return false;
+        }else{
+            visited[i][j] = true;
+        }
+        boolean ans = dfs(chars,str,i-1,j,index+1,visited)||dfs(chars,str,i+1,j,index+1,visited)
+                ||dfs(chars,str,i,j-1,index+1,visited)||dfs(chars,str,i,j+1,index+1,visited);
+        visited[i][j] = false;
+        return ans;
     }
 }
